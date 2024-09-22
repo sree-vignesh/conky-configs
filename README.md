32ab6558d37526808e9826880473af08bcbf4ded
Sree Vignesh <sreevignesh256@gmail.com>
2024-09-22 17:11:21 +0530

    Added a startup script.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # conky-configs
+
+This repo contains various Conky config files.
+
+Feel free to tweak any sections to better match your project’s specifics or your personal touch! If you need any more help or further adjustments, let me know!
---
+++ conky-startup.sh
@@ -0,0 +1,8 @@
+sleep 20s
+killall conky
+cd "/home/wikki/.conky/TeejeeTech"
+conky -c "/home/wikki/.conky/TeejeeTech/Process Panel" &
+cd "/home/wikki/.conky"
+conky -c "/home/wikki/.conky/mine" &
+cd "/home/wikki/.conky"
+conky -c "/home/wikki/.conky/quote" &
